71768ad6d7d618e5ac8e67e4bfddc66416a1efe2
Lily <lilyeac@hotmail.com>
2022-06-24 22:57:34 -0500

    sem06

--- src/app/pages/project/project.component.ts
+++ src/app/pages/project/project.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { ApartmentService } from 'src/app/services/apartment.service';
 import { ProjectService } from 'src/app/services/project.service';
 
 @Component({
@@ -11,31 +12,34 @@ export class ProjectComponent implements OnInit {
 
   constructor(
     private readonly ps: ProjectService,
-    private ar:ActivatedRoute
-
+    private readonly as: ApartmentService,
+    private ar: ActivatedRoute
   ) { }
-  project: any=[]
 
-__getProject_byId(idproject:number){
-this.ps.getProjects().subscribe((rest:any)=>{
-this.project=rest.data.filter((item:{id:number})=> item.id == idproject)
-console.log(this.project)
-})
-}
+  project: any = []
+  apartments: any = []
 
-__getApartments_byIdProject(idproject:number){
-  this.ps.getProjects().subscribe((rest:any)=>{
-    this.project=rest.data.filter((item:{id:number})=>item.)
+  __getProject_byId(idproject: number) {
+    this.ps.getProjects().subscribe((rest: any) => {
+      this.project = rest.data.filter((item: {id: number}) => item.id == idproject)
+      // console.log(this.project)
+    })
   }
-}
 
-
+  __getApartments_byIdProject(idproject: number) {
+    this.as.getApartments().subscribe((rest: any) => {
+      this.apartments = rest.data.filter((item: {projectId: number}) => item.projectId == idproject)
+      // console.log(this.apartments)
+    })
+  }
 
   ngOnInit(): void {
-    this.ar.params.subscribe((p: Params)=>{
-      if(p["id"]){
+    this.ar.params.subscribe((p: Params) => {
+      if(p["id"]) {
         this.__getProject_byId(p["id"])
+        this.__getApartments_byIdProject(p["id"])
       }
     })
   }
+
 }
